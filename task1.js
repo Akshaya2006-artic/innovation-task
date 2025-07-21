a853a2e0ab6b63fa40d8a3b00b45688280fd143e
Akshaya2006-artic <akshayaashok2025@gmail.com>
2025-07-21 07:06:21 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,11 @@
+ var num1 = 7;
+var num2 = 3;
+console.log(num1 + num2);
+
+let value1 = 25;
+let value2 = 15;
+console.log(value1 + value2);
+
+const a = 8;
+const b = 6;
+console.log(rate * time);
